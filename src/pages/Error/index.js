3ec9a660bb76e4aa541de1cbae25cc8fb075605b
Helena Paiva <hellenapaiva@msn.com>
2022-02-02 15:35:27 -0300

    adicionado pagina 404

---
+++ src/pages/Error/index.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import "./error.css";
+
+const Error = () => {
+    return (
+        <div className="container-error">
+            <img src="notfound.png" alt="Página não encontrada" />
+            <h1>Página não encontrada</h1>
+            <Link to="/">
+                Voltar para home
+            </Link>
+        </div>
+    );
+};
+
+export default Error;
